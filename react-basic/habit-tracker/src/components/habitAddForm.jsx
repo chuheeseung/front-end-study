5aabb53071c7f91af6788ebf0895195cd9f59767
chuheeseung <youverygood@naver.com>
2022-01-17 18:16:11 +0900

    22.01.17 / ~4.17

--- react-basic/habit-tracker/src/components/habitAddForm.jsx
+++ react-basic/habit-tracker/src/components/habitAddForm.jsx
@@ -1,6 +1,35 @@
-import React, { PureComponent } from 'react';
+import React, { Component, memo } from 'react';
 
-class HabitAddForm extends PureComponent {
+// Function Component
+const HabitAddForm = memo((props) => {
+    const formRef = React.createRef();
+    const inputRef = React.createRef();
+
+    const onSubmit = (event) => {
+        event.preventDefault(); // 새로고침 방지
+        const name = inputRef.current.value; 
+        name && props.onAdd(name);
+        formRef.current.reset();
+    };
+
+    return (
+            <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+                <input 
+                    ref={inputRef} 
+                    type="text" 
+                    className="add-input" 
+                    placeholder="Habit" 
+                />
+                <button className="add-button">Add</button>
+            </form>
+        );
+});
+
+export default HabitAddForm;
+
+
+/*  // Class Component
+class HabitAddForm extends Component {
     formRef = React.createRef();
     inputRef = React.createRef();
 
@@ -28,3 +57,4 @@ class HabitAddForm extends PureComponent {
 }
 
 export default HabitAddForm;
+*/
