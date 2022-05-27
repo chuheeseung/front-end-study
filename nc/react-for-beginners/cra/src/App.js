4488b86a6e7f8ffd80b1363f0f42ce8a70abf8a7
heeseungchu <youverygood@naver.com>
2022-05-28 01:12:04 +0900

    22.05.27

--- nc/react-for-beginners/cra/src/App.js
+++ nc/react-for-beginners/cra/src/App.js
@@ -1,11 +1,40 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => {setKeyword(event.target.value)};
+
+  console.log("I run all the time");
+  const iRunOnlyOnce = () => {console.log("I run only once");};
+
+  // useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log("call the api");
+  }, []);
+
+  useEffect(() => {
+    // if(keyword !== "" && keyword.length >= 5) {
+    //   console.log("Search for ", keyword);
+    // }
+    console.log("I run when 'keyword' changes");
+  }, [keyword]);
+
+  // console.log("Search for ", keyword);
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"}/>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here ..." />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>Click Me</button>
     </div>
   );
 }
