8564edc5710dc46bd1df61fc59f079897caba479
heeseungchu <youverygood@naver.com>
2022-06-30 19:18:17 +0900

    22.06.30

---
+++ umc/react-study/review/week01/src/movieDummy.js
@@ -0,0 +1,388 @@
+export const dummy = {
+    dates: {
+    maximum: "2022-07-05",
+    minimum: "2022-05-18"
+    },
+    page: 1,
+    results: [
+    {
+    adult: false,
+    backdrop_path: "/wo3l9p0S7pcvwlzlndtKgq0peRJ.jpg",
+    genre_ids: [
+    28,
+    12,
+    878
+    ],
+    id: 507086,
+    original_language: "en",
+    original_title: "Jurassic World Dominion",
+    overview: "공룡들의 터전이었던 이슬라 누블라 섬이 파괴된 후, 마침내 공룡들은 섬을 벗어나 세상 밖으로 출몰한다. 지상에 함께 존재해선 안 될 위협적 생명체인 공룡의 등장으로 인류 역사상 겪어보지 못한 사상 최악의 위기를 맞이한 인간들. 지구의 최상위 포식자 자리를 걸고 인간과 공룡의 최후의 사투가 펼쳐진다.",
+    popularity: 1976.615,
+    poster_path: "/odxdUZWZ7fBfy3ZRj063wuJnZvo.jpg",
+    release_date: "2022-06-01",
+    title: "쥬라기 월드: 도미니언",
+    video: false,
+    vote_average: 6.7,
+    vote_count: 868
+    },
+    {
+    adult: false,
+    backdrop_path: "/odJ4hx6g6vBt4lBWKFD1tI8WS4x.jpg",
+    genre_ids: [
+    28,
+    18
+    ],
+    id: 361743,
+    original_language: "en",
+    original_title: "Top Gun: Maverick",
+    overview: "최고의 파일럿이자 전설적인 인물 매버릭은 자신이 졸업한 훈련학교 교관으로 발탁된다. 그의 명성을 모르던 팀원들은 매버릭의 지시를 무시하지만 실전을 방불케 하는 상공 훈련에서 눈으로 봐도 믿기 힘든 전설적인 조종 실력에 모두가 압도된다. 매버릭의 지휘 아래 견고한 팀워크를 쌓아가던 팀원들에게 국경을 뛰어넘는 위험한 임무가 주어지자 매버릭은 자신이 가르친 동료들과 함께 마지막이 될지 모를 하늘 위 비행에 나서는데…",
+    popularity: 1034.136,
+    poster_path: "/jMLiTgCo0vXJuwMzZGoNOUPfuj7.jpg",
+    release_date: "2022-06-22",
+    title: "탑건: 매버릭",
+    video: false,
+    vote_average: 8.4,
+    vote_count: 1342
+    },
+    {
+    adult: false,
+    backdrop_path: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg",
+    genre_ids: [
+    53
+    ],
+    id: 924482,
+    original_language: "en",
+    original_title: "The Ledge",
+    overview: "친구와 함께 암벽등반을 간 켈리는 그곳에서 만난 남성들에게 친구가 살해당하는 것을 목격하고 카메라로 찍는데… 쫓아오는 그들을 피할 곳은 눈앞의 거대한 암벽뿐...",
+    popularity: 567.969,
+    poster_path: "/qXU3lr4YmyT8y6kaX1CcpRiqia2.jpg",
+    release_date: "2022-06-15",
+    title: "더 렛지",
+    video: false,
+    vote_average: 6.2,
+    vote_count: 27
+    },
+    {
+    adult: false,
+    backdrop_path: "/wgdWNBqPIAH29ZsL3I0AVx7XRed.jpg",
+    genre_ids: [
+    28,
+    9648,
+    53
+    ],
+    id: 615173,
+    original_language: "ko",
+    original_title: "마녀(魔女) Part2. The Other One",
+    overview: "자윤이 사라진 뒤, 정체불명의 집단의 무차별 습격으로 마녀 프로젝트가 진행되고 있는 아크가 초토화된다. 그곳에서 홀로 살아남은 소녀는 생애 처음 세상 밖으로 발을 내딛고 우연히 만난 경희의 도움으로 농장에서 지내며 따뜻한 일상에 적응해간다. 한편, 소녀가 망실되자 행방을 쫓는 책임자 장과 마녀 프로젝트의 창시자 백총괄의 지령을 받고 제거에 나선 본사 요원 조현, 경희의 농장 소유권을 노리는 조직의 보스 용두와 상해에서 온 의문의 4인방까지 각기 다른 목적을 지닌 세력이 하나 둘 모여들기 시작하면서 소녀 안에 숨겨진 본성이 깨어나는데…",
+    popularity: 146.918,
+    poster_path: "/cah6iIYQBctvCmBjsKGb1NCiygr.jpg",
+    release_date: "2022-06-15",
+    title: "마녀(魔女) Part2. The Other One",
+    video: false,
+    vote_average: 6.3,
+    vote_count: 3
+    },
+    {
+    adult: false,
+    backdrop_path: "/ic8h5oYQWwqQWPyfpZUSomZTdao.jpg",
+    genre_ids: [
+    18,
+    53
+    ],
+    id: 821427,
+    original_language: "en",
+    original_title: "The Novice",
+    overview: "대학 신입생 알렉스는 교내 조정부에 가입한 후 동급생 제이미에게 경쟁심을 느낀다. 늘 최고를 갈망하는 알렉스는 팀 1군에 들기 위해 훈련을 거듭하고, 스스로를 극한으로 내몰기 시작하는데···",
+    popularity: 73.088,
+    poster_path: "/xjd6s1jgB1rwKQsiHv995aQbamY.jpg",
+    release_date: "2022-05-25",
+    title: "더 노비스",
+    video: false,
+    vote_average: 6.5,
+    vote_count: 58
+    },
+    {
+    adult: false,
+    backdrop_path: "/A1bWhTFQKkhF1yhSKWosSyzn2Hp.jpg",
+    genre_ids: [
+    80,
+    18,
+    10749,
+    9648
+    ],
+    id: 705996,
+    original_language: "ko",
+    original_title: "헤어질 결심",
+    overview: "산 정상에서 추락한 한 남자의 변사 사건. 담당 형사 해준은 사망자의 아내 서래와 마주하게 된다. 남편의 죽음 앞에서 특별한 동요를 보이지 않는 서래. 경찰은 보통의 유가족과는 다른 서래를 용의선상에 올린다. 해준은 사건 당일의 알리바이 탐문과 신문, 잠복수사를 통해 서래를 알아가면서 그녀에 대한 관심이 점점 커져가는 것을 느낀다. 한편, 좀처럼 속을 짐작하기 어려운 서래는 상대가 자신을 의심한다는 것을 알면서도 조금의 망설임도 없이 해준을 대하는데...",
+    popularity: 68.315,
+    poster_path: "/wKArLUTzdWRg0Y3sIlgwY7BQ2u8.jpg",
+    release_date: "2022-06-29",
+    title: "헤어질 결심",
+    video: false,
+    vote_average: 7.9,
+    vote_count: 8
+    },
+    {
+    adult: false,
+    backdrop_path: "/bIFrsAcqkLUUxaaiqqpFexYPeCh.jpg",
+    genre_ids: [
+    14,
+    27,
+    53
+    ],
+    id: 532710,
+    original_language: "en",
+    original_title: "Firestarter",
+    overview: "평범한 사람들은 없는 특별한 능력을 가졌다는 이유만으로 과거 정부의 비밀 실험 기관인 ‘더 샵’에 의해 이용당했던 ‘앤디’와 ‘비키’는 ‘더 샵’에서 가까스로 탈출한 뒤 딸 ‘찰리’를 낳고 조용한 나날을 보내고 있다. 시간이 흘러 말로는 설명할 수 없는 내면의 힘을 느끼기 시작한 ‘찰리’는 어느 날 학교에서 한 남학생에게 괴롭힘을 당하고 순간적인 분노에 휩싸여 숨어 들어간 화장실을 파괴하는 지경에 이른다. 이로 인해 ‘더 샵’에서 개편된 비밀 기관인 ‘DSI’가 ‘찰리’의 존재를 알게 되고 또 다른 초능력자 ‘레인버드’에게 이들을 잡아오라는 명령을 내린다. ‘찰리’는 통제할 수 없는 엄청난 힘의 발현에 혼란스러워 하고 그런 딸을 보호하기 위해 ‘앤디’와 ‘비키’는 고군분투하지만 ‘레인버드’를 앞세운 ‘DSI’가 이들의 숨통을 빠르게 조여 오기 시작하는데…",
+    popularity: 63.701,
+    poster_path: "/ko1iKcUp4iIjTNAYFOWFVG8tUi7.jpg",
+    release_date: "2022-05-19",
+    title: "파이어스타터",
+    video: false,
+    vote_average: 6.1,
+    vote_count: 259
+    },
+    {
+    adult: false,
+    backdrop_path: "/ejkbJM697dNrtQvIiUpeUPGpoXX.jpg",
+    genre_ids: [
+    18
+    ],
+    id: 632617,
+    original_language: "en",
+    original_title: "C'mon C'mon",
+    overview: "어린이의 삶과 미래에 대해 인터뷰하는 라디오 저널리스트 조니. 어머니의 죽음 이후 오랜 시간 등돌리고 살던 여동생 비브의 부탁으로 육아 난이도 극상에 어디로 튈지 모르는 8살 조카 제시를 돌보게 된다. 뜻하지 않게 즐거운 시간과 슬픔의 시간, 조용한 밤과 흥미로운 낮 시간을 함께하게 된 둘. 조니는 제시에게도 인터뷰를 시도하지만 도리어 질문 세례를 받고 마음 깊은 곳에 숨겨둔 과거를 꺼내게 되는데... 이 변화무쌍한 세상 속에 어쩐지 우리, 진짜 가족이 된 것만 같다!",
+    popularity: 56.811,
+    poster_path: "/9PXGqluqPfD024s8gERpo6MD3IF.jpg",
+    release_date: "2022-06-30",
+    title: "컴온 컴온",
+    video: false,
+    vote_average: 7.6,
+    vote_count: 300
+    },
+    {
+    adult: false,
+    backdrop_path: "/iuM006FCrVXqvwLg4R0hkST0HPy.jpg",
+    genre_ids: [
+    80,
+    28
+    ],
+    id: 619803,
+    original_language: "ko",
+    original_title: "범죄도시 2",
+    overview: "가리봉동 소탕작전 후 4년 뒤, 금천서 강력반은 베트남으로 도주한 용의자를 인도받아 오라는 미션을 받는다. 괴물형사 마석도와 전일만 반장은 현지 용의자에게서 수상함을 느끼고, 그의 뒤에 무자비한 악행을 벌이는 강해상이 있음을 알게 된다. 마석도와 금천서 강력반은 한국과 베트남을 오가며 역대급 범죄를 저지르는 강해상을 본격적으로 쫓기 시작하는데...나쁜 놈들 잡는 데 국경 없다! 통쾌하고 화끈한 범죄 소탕 작전이 다시 펼쳐진다!",
+    popularity: 51.55,
+    poster_path: "/9oonDgLkh42GsLkbVmW7nU1ib6u.jpg",
+    release_date: "2022-05-18",
+    title: "범죄도시 2",
+    video: false,
+    vote_average: 6.5,
+    vote_count: 16
+    },
+    {
+    adult: false,
+    backdrop_path: "/dom8lzl2iU46nDu6s4lBolBNjQs.jpg",
+    genre_ids: [
+    18
+    ],
+    id: 736732,
+    original_language: "ko",
+    original_title: "브로커",
+    overview: "세탁소를 운영하지만 늘 빚에 시달리는 상현과 베이비 박스 시설에서 일하는 보육원 출신의 동수. 거센 비가 내리는 어느 날 밤, 그들은 베이비 박스에 놓인 한 아기를 몰래 데려간다. 하지만 이튿날, 생각지 못하게 엄마 소영이 아기 우성을 찾으러 돌아온다. 아기가 사라진 것을 안 소영이 경찰에 신고하려 하자 솔직하게 털어놓는 두 사람. 우성이를 잘 키울 적임자를 찾아 주기 위해서 그랬다는 변명이 기가 막히지만 소영은 우성이의 새 부모를 찾는 여정에 상현, 동수와 함께하기로 한다. 한편 이 모든 과정을 지켜본 형사 수진과 후배 이형사. 반 년째 이어온 수사를 마무리할 수 있는 결정적 증거를 포착하기 위해 이들의 뒤를 조용히 쫓는다.",
+    popularity: 21.18,
+    poster_path: "/qJ1gDx7BkJ4Pjfjd3Ulh0WzL3zv.jpg",
+    release_date: "2022-06-08",
+    title: "브로커",
+    video: false,
+    vote_average: 7.2,
+    vote_count: 7
+    },
+    {
+    adult: false,
+    backdrop_path: null,
+    genre_ids: [
+    28
+    ],
+    id: 980021,
+    original_language: "ko",
+    original_title: "개들의 도시",
+    overview: "평범하게 살고 싶었던 훈이. 하지만, 세상은 그를 평범하게 살도록 내버려두지 않는다. 교도소에서 출소한 훈이는 단속반원들과 실랑이를 벌이는 어머니를 돕다가 경찰서로 끌려간다. 합의금을 내기 위해 돈을 받으러 준기에게 받은 대기의 연락처를 들고 그를 찾아가 함께 일을 하게 된다. 어머니의 식당 단골인 정화는 자신과 남동생을 담보로 아버지가 도박빚을 남긴 채 도주한 사실을 알게되고 의지할 곳 없는 그녀는 훈이의 어머니에게 찾아와 세 사람의 동거가 시작된다. 한편 비리경찰 영태는 준기를 깡패로 깔아뭉개며 뒤로는 범죄를 꾸미는데...",
+    popularity: 24.514,
+    poster_path: "/j3fpKs0Ht50BGCWEKuodD04oMgK.jpg",
+    release_date: "2022-06-29",
+    title: "개들의 도시",
+    video: false,
+    vote_average: 0,
+    vote_count: 0
+    },
+    {
+    adult: false,
+    backdrop_path: "/2kj6FOo0B9NtCen1Ta7LoaPW9wG.jpg",
+    genre_ids: [
+    18,
+    878
+    ],
+    id: 585378,
+    original_language: "en",
+    original_title: "After Yang",
+    overview: "함께 살던 안드로이드 인간 양이 어느 날 작동을 멈추자 제이크 가족은 그를 수리할 방법을 찾는다. 그러던 중, 양에게서 특별한 메모리 뱅크를 발견하고 그의 기억을 탐험하기 시작하는데…",
+    popularity: 18.385,
+    poster_path: "/tDeH8dFeKpHQCRFbs6So05qp6ZJ.jpg",
+    release_date: "2022-06-01",
+    title: "애프터 양",
+    video: false,
+    vote_average: 6.8,
+    vote_count: 86
+    },
+    {
+    adult: false,
+    backdrop_path: "/aSgFDRnfppKxRGZE8BWOfgzAR3k.jpg",
+    genre_ids: [
+    28,
+    53,
+    27
+    ],
+    id: 523638,
+    original_language: "en",
+    original_title: "Prisoners of the Ghostland",
+    overview: "붉은 조명이 드리운 스산한 사무라이 마을. 신비로운 소녀 ‘버니스’와 그녀의 친구들이 한밤중 몰래 탈출을 시도한다. 사라진 그녀를 찾기 위해 사무라이 마을의 총독은 ‘히어로’를 고용한다. 그가 도착한 곳은 오래전 끔찍한 폭발로 시간이 멈추고 희망이 사라진 황폐한 마을 ‘고스트랜드’ 그곳에서 목소리를 잃은 ‘버니스’를 찾은 ‘히어로’는 ‘고스트랜드’의 진짜 정체를 알게 되는데…",
+    popularity: 19.778,
+    poster_path: "/xZtWqaByP0ov8Rx5sE0MVOuxOQP.jpg",
+    release_date: "2022-06-29",
+    title: "고스트랜드",
+    video: false,
+    vote_average: 5,
+    vote_count: 311
+    },
+    {
+    adult: false,
+    backdrop_path: null,
+    genre_ids: [
+    99
+    ],
+    id: 974721,
+    original_language: "ko",
+    original_title: "위대한 침묵",
+    overview: "이 다큐 영화는 대한민국 제18대 대통령이자 최초의 여성 대통령 박근혜의 탄핵과 형사재판의 불법, 부당성을 기록하는 작품이다. 박근혜 대통령 탄핵 및 형사사건의 변호인단, 법대 교수, 정치학 교수들의 인터뷰와 사건 현장에 대한 촬영을 근거로 화면을 구성하였다. 탄핵사건의 도화선으로 작용한 투기자본감시센터 고발장의 진실은 검찰수사기록을 통해, JTBC의 태블릿PC날조보도는 국립과학수사연구원의 포렌식 결과물을 통해 규명했다.",
+    popularity: 23.226,
+    poster_path: "/djDf20ylZYQoiN3QWjbfOM16FDq.jpg",
+    release_date: "2022-06-30",
+    title: "위대한 침묵",
+    video: false,
+    vote_average: 0,
+    vote_count: 0
+    },
+    {
+    adult: false,
+    backdrop_path: "/cv6Nn8Yt9mgFoUSievEdW9JK82A.jpg",
+    genre_ids: [
+    18
+    ],
+    id: 423333,
+    original_language: "en",
+    original_title: "Mass",
+    overview: "돌이킬 수 없는 사건으로 아이를 잃은 두 부부의 슬픔, 분노, 절망, 후회가 폭발하는 111분의 마스터피스",
+    popularity: 10.203,
+    poster_path: "/25hs4cufSf4bAfwjJbTZbEKWyZg.jpg",
+    release_date: "2022-05-18",
+    title: "매스",
+    video: false,
+    vote_average: 7.8,
+    vote_count: 118
+    },
+    {
+    adult: false,
+    backdrop_path: "/w3sqSIZDQLthCPpOCkcN5CkLRYM.jpg",
+    genre_ids: [
+    35
+    ],
+    id: 712150,
+    original_language: "fr",
+    original_title: "Le Discours",
+    overview: "헤어지고 38일째, 소니아에게 문자를 보냈다. 소니아의 답장은 여전히 오지 않고 눈치 없는 누나와 예비 매형이 나에게 결혼식 축사를 부탁한다. 다양한 방법으로 축사를 망치고 모두의 원망을 듣는 나의 미래가 눈앞에 보이는 것만 같아 두렵다. 그나저나 소니아는 왜 문자 답장이 없을까?",
+    popularity: 6.114,
+    poster_path: "/eLY5SSlDMmmaAebOXoSauasHvpU.jpg",
+    release_date: "2022-05-19",
+    title: "완벽한 축사를 준비하는 방법",
+    video: false,
+    vote_average: 6.5,
+    vote_count: 184
+    },
+    {
+    adult: false,
+    backdrop_path: "/eDRQBrc3KVTurfnAWWZWs6l35VE.jpg",
+    genre_ids: [
+    28
+    ],
+    id: 952888,
+    original_language: "ko",
+    original_title: "피는 물보다 진하다",
+    overview: "최대 청부폭력조직 '백정파'는 무자비함으로 악명 높은 해결사, 일명 '도깨비'를 앞세워 일대를 장악한다. 그러나, 베일에 싸인 ‘도깨비’ 두현은 친형제 같았던 영민의 죄를 뒤집어쓰고 10년의 수감생활을 하게 된다. 한편, 두현이 사라진 사이, ‘도깨비＇행세를 하며 조직을 차지한 영민은 두현의 출소 소식을 듣고 불안에 휩싸인다. 영민은 두현을 먼저 치기로 하고 새 삶을 시작하려던 두현은 결국, 진짜 ‘도깨비’의 부활을 선언하며 영민과 조직을 상대로 싸움을 시작하는데...",
+    popularity: 8.759,
+    poster_path: "/1pzF971jRJuaHoMscOmQhwQ0TWJ.jpg",
+    release_date: "2022-05-25",
+    title: "피는 물보다 진하다",
+    video: false,
+    vote_average: 5,
+    vote_count: 1
+    },
+    {
+    adult: false,
+    backdrop_path: "/sXyy9VSf5XphIM5Q0HawcTA2Xeo.jpg",
+    genre_ids: [
+    18
+    ],
+    id: 785209,
+    original_language: "fr",
+    original_title: "Un monde",
+    overview: "막 학교에 입학한 일곱 살 노라는 낯선 학교가 무섭다. 아벨은 동생 노라를 위로하지만 사실 아벨도 학교가 두렵다. 점차 친구도 사귀며 학교생활에 적응해 가는 노라는 우연히 아벨이 친구들에게 괴롭힘 당하는 걸 목격한다. 노라는 어른들에게 이 사실을 알리려 하지만 아벨은 노라가 그저 침묵하길 바라는데…",
+    popularity: 5.407,
+    poster_path: "/iCdPMOpNwnkyZsSdCQ90Bugseys.jpg",
+    release_date: "2022-05-25",
+    title: "플레이그라운드",
+    video: false,
+    vote_average: 7.3,
+    vote_count: 67
+    },
+    {
+    adult: false,
+    backdrop_path: "/lCd1DWvePNruFx6gsMRU3Wc8X6Y.jpg",
+    genre_ids: [
+    27,
+    53,
+    9648
+    ],
+    id: 896448,
+    original_language: "en",
+    original_title: "Beneath the Surface",
+    overview: "즐거운 가족여행에서 자신의 실수로 요트에서 떨어져 아버지와 동생을 잃은 클로이는 자신의 탓이라 자책하며 지낸다. 그런 클로이를 안타까워하던 친구 에이미는 그녀에게 심리상담을 권하지만, 남자친구인 아이작과 새엄마인 비키는 심리상담을 받아도 달라지는 건 없다며 그냥 쉴 것을 권한다. 클로이는 새엄마와 남자친구 몰래 상담을 받으러 다니고, 상담사와 얘기하며 그날의 기억이 떠오르는데...",
+    popularity: 8.134,
+    poster_path: "/mQHsegbsCAnksMnbtqDFDMw1RIz.jpg",
+    release_date: "2022-07-05",
+    title: "언더 샤크",
+    video: false,
+    vote_average: 0,
+    vote_count: 0
+    },
+    {
+    adult: false,
+    backdrop_path: "/dRKWAdh2zrXFDSK17IuS4V30Mo3.jpg",
+    genre_ids: [
+    18
+    ],
+    id: 982834,
+    original_language: "ko",
+    original_title: "The Boxer",
+    overview: "‘동석’(결휘)은 복싱 전국체전에서 우승하지만 ‘청무관’의 관장이자 아버지인 ‘순일’(문창길)의 후광으로 인한 승부조작이라는 오명을 쓰고 사람들에게 인정받지 못한다. 그렇게 체육관을 뛰쳐나와 3류 양아치 같은 삶을 살던 그는 어느 날 아버지가 죽었다는 소식을 듣게 되고, 아버지의 마지막 유산인 ‘청무관’의 간판을 지키기 위해 가장 무모한 시합을 준비하게 된다!",
+    popularity: 4.319,
+    poster_path: "/qKvCCMIvexxNnZRcFi5Eb0vM10S.jpg",
+    release_date: "2022-06-23",
+    title: "갓길로 달리는 코뿔소",
+    video: false,
+    vote_average: 5,
+    vote_count: 1
+    }
+    ],
+    total_pages: 3,
+    total_results: 45
+    }
