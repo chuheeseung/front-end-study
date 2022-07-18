59caada6c9f844f4136b0f384ebeb3dd1c535c6c
heeseungchu <youverygood@naver.com>
2022-07-18 23:40:01 +0900

    22.07.18

---
+++ inflearn/react-firebase-chat-app/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+import ChatPage from './components/ChatPage/ChatPage';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<ChatPage />} />
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
