de0ad121550f9a73bd598fbcb045372ddd347274
chuheeseung <youverygood@naver.com>
2022-10-30 00:04:29 +0900

    22.10.29 ~4.9

---
+++ 00.REACT-STUDY/dc/new-version/basic/src/AppJSX.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function AppJSX() {
+	const name = '백현이';
+	return (
+		<>
+			<h1 className="orange">{name}</h1>
+			<h2>Hello!</h2>
+		</>
+	);
+}
+
+export default AppJSX;
