afc5c2bc299208975f248e9e7454448c2dd7898c
chuheeseung <youverygood@naver.com>
2023-01-05 17:46:27 +0900

    23.01.05 ~12.9

--- 00.REACT-STUDY/dc/new-version/youtube/src/App.js
+++ 00.REACT-STUDY/dc/new-version/youtube/src/App.js
@@ -1,5 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Outlet } from 'react-router-dom';
+import SearchHeader from './components/SearchHeader';
+
+const queryClient = new QueryClient();
+
 function App() {
-	return <></>;
+	return (
+		<>
+			<SearchHeader />
+			<QueryClientProvider client={queryClient}>
+				<Outlet />
+			</QueryClientProvider>
+		</>
+	);
 }
 
 export default App;
